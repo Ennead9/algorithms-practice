9f43678df4e8e655f74916d780c8c1d1d819bf9f
Isaac_R <idrfritcher@gmail.com>
2023-11-05 17:15:42 -0600

    Start

---
+++ hw2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int function(int x)
+{
+
+}
+
+int main()
+{
+    int n = 50; // Size for data set
+    int arr[n]; // Array of size n
+    
+    // Loop over n
+    for(int i = 0; i < n; i++)
+    {
+        
+    }
+}
