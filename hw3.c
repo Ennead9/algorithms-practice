8c77221e31e4d2795cc746a2ed5ca17d986fd1a6
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-23 20:28:27 -0600

    Remove hw2.c, add hw3.c (works)

---
+++ hw3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *first, int *second){
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+// Find pivot and sort array
+int partition(int arr[], int lower, int upper){
+    
+    int pivot = arr[upper]; // Set pivot (n-1)
+    int i = (lower - 1);
+    int j;
+
+    // Loop over array to compare against pivot val
+    for(j = lower; j < upper; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[upper]); // Swap pivot so it's in the right spot
+
+    return i;
+}
+// Recursive quickSort function
+void quickSort(int arr[], int lower, int upper)
+{
+    int partitionIndex = partition(arr, lower, upper);
+
+    if(upper > lower){
+        
+        int partitionIndex = partition(arr, lower, upper);
+
+        quickSort(arr, lower, partitionIndex-1);
+        quickSort(arr, partitionIndex + 1, upper);
+    }
+}
+
+int main()
+{
+    // Create data set
+    int data[7] = {3, 6, 7, 5, 1, 2, 4};
+
+    printf("Before: ");
+    for(int i = 0; i < 7; i++){
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    // Call quicksort
+    quickSort(data, 0, 6);
+
+    printf("\nAfter: ");
+    for(int i = 0; i < 7; i++){
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
