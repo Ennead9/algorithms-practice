b91811b80a01bd6ca33538d6dc6c7e997707eb4a
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-24 22:28:25 -0600

    Nada

---
+++ HW4/hw4.py
@@ -0,0 +1,24 @@
+# Linear Sort Algorithm
+# Sorting pixels based on color values?
+
+def main():
+
+    # Create static list of RGB values
+    
+    # Create test list & print
+    data = [170, 45, 75, 90, 802, 24, 2, 66]
+    print(f"Unsorted: {data}\n")
+
+    digits = len(str(max(data)))
+
+    print(digits)
+    
+    for i in range(3):
+        print("loopy oney")
+
+        for i in data:
+            print("loopy twoey")
+
+
+if __name__ == "__main__":
+    main()
