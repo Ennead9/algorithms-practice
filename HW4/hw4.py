b817acb498f3a0dd7070134b33b7cb850c261515
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-25 00:09:13 -0600

    Start countingSort alg'

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,24 +1,33 @@
 # Linear Sort Algorithm
 # Sorting pixels based on color values?
 
+def countSort(arr):
+    maxVal = max(arr)
+    digits = len(str(maxVal))
+    print(f"Digits: {digits} maxVal: {maxVal}")
+
+    outputArr = [0 for i in range(maxVal+1)]
+    countArr = [0 for i in range(maxVal+1)]
+    
+    for i in arr:
+        countArr[i] += 1
+    for i in arr:
+        countArr[i] = countArr[i - 1] + countArr[i]
+    
+    for i in reversed(arr):
+        print(i)
+        print(arr[i])
+        outputArr[countArr[arr[i]]-1] = arr[i]
+
 def main():
 
     # Create static list of RGB values
     
     # Create test list & print
-    data = [170, 45, 75, 90, 802, 24, 2, 66]
+    # data = [170, 45, 75, 90, 802, 24, 2, 66]
+    data = [0, 3, 4, 6, 3, 2, 11]
     print(f"Unsorted: {data}\n")
-
-    digits = len(str(max(data)))
-
-    print(digits)
-    
-    for i in range(3):
-        print("loopy oney")
-
-        for i in data:
-            print("loopy twoey")
-
+    countSort(data)
 
 if __name__ == "__main__":
     main()
