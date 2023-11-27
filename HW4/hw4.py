7e72b9eb0921583702b9fc38554387cb7322c9ba
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-27 12:05:25 -0600

    Stuff

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -6,18 +6,29 @@ def countSort(arr):
     digits = len(str(maxVal))
     print(f"Digits: {digits} maxVal: {maxVal}")
 
-    outputArr = [0 for i in range(maxVal+1)]
     countArr = [0 for i in range(maxVal+1)]
     
+    # Count of unique elements in countArr
     for i in arr:
         countArr[i] += 1
-    for i in arr:
+    
+    print(f"countArr: {countArr}")
+    print("-----------")
+    # Modify freq. array to store running sum of its own elements
+    for i in range(1, len(countArr)):
         countArr[i] = countArr[i - 1] + countArr[i]
     
-    for i in reversed(arr):
-        print(i)
-        print(arr[i])
-        outputArr[countArr[arr[i]]-1] = arr[i]
+    print(f"countArr: {countArr}")
+
+    # Create output array (list) with length equal to "# of elements"
+    outputArr = [0 for i in range(countArr[-1])]
+    print(outputArr)
+
+    # Put stuff into output array
+    # Put 
+    for i in range(1, len(arr)):
+        print(countArr[i])
+    
 
 def main():
 
@@ -25,7 +36,7 @@ def main():
     
     # Create test list & print
     # data = [170, 45, 75, 90, 802, 24, 2, 66]
-    data = [0, 3, 4, 6, 3, 2, 11]
+    data = [0, 3, 4, 6, 3, 2, 7]
     print(f"Unsorted: {data}\n")
     countSort(data)
 
