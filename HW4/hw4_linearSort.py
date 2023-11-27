0b9fff07ff90f1b3e57d445c048c1492818e1b11
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-27 13:42:47 -0600

    Code cleanup before implementing loop

--- HW4/hw4_linearSort.py
+++ HW4/hw4_linearSort.py
@@ -3,51 +3,42 @@
 def countSort(arr):
     maxVal = max(arr)
     digits = len(str(maxVal))
-    print(f"Digits: {digits} maxVal: {maxVal}")
-
+    #print(f"Digits: {digits} maxVal: {maxVal}")
     countArr = [0 for i in range(maxVal+1)]
 
     # 1. Frequency array for count of unique elements in input array   
     for i in arr:
         countArr[i] += 1
 
-    print(f"countArr: {countArr}")
-    print("------------")
-
     # 2. Modify freq array to store running sum of its own elements
     for i in range(1, len(countArr)):
         countArr[i] = countArr[i - 1] + countArr[i]
-    
-    print(f"countArr: {countArr}")
  
     # 3. Create output array (list) with len equal to # of elements
     outputArr = [0 for i in range(countArr[-1])]
-    print(f"Initial outputArr: {outputArr}")
 
-    print(arr)
     # Traverse input array from end to preserve stability
     for i in range(len(arr) - 1, -1, -1):
 
         # Value at countArr[arr[i]] represents cumulative count of the element arr[i] in original array
-        # The cumulative count indicates total number of elements in the original array that are
-        # less than or equal to arr[i]
+        # Cumulative count indicates total # of elements in original array that are less than or equal to arr[i]
         # -1 part is to adjust for zero-based indexing
         # EXAMPLE:
         # If arr[i] is 3, and countArr[i] is 5, this means there are 5 elements in arr that are <= 3
-        # To place 3 in the sorted array, you need to put it in the 4th position, so
-        # countArr[3] - 1 = 4
+        # To place 3 in sorted array, need to put it in the 4th position, so: countArr[3] - 1 = 4
         outputArr[countArr[arr[i]] - 1] = arr[i]
         countArr[arr[i]] -= 1 # need to decrement the cumulative count since we've already placed a number
-    print(f"FINAL SORTED (outputArr): {outputArr}")
 
+    # Print sorted array
+    print(f"Sorted array: {outputArr}")
 
 def main():
 
     # Create test list & print
-    data = [0, 3, 4, 6, 3, 2, 7]
-    test2 = [3, 6, 6, 3, 1, 0, 1]
+    data = [1, 0, 3, 4, 6, 3, 2, 7]
     print(f"Unsorted: {data}\n")
     countSort(data)
 
+    
 if __name__ == "__main__":
     main()
