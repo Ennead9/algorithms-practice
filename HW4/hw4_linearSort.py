b691fd7cca86dd1a0b85b981a615e21873633ed7
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-27 13:46:07 -0600

    Final cleanup before moving on to loop

--- HW4/hw4_linearSort.py
+++ HW4/hw4_linearSort.py
@@ -2,8 +2,6 @@
 
 def countSort(arr):
     maxVal = max(arr)
-    digits = len(str(maxVal))
-    #print(f"Digits: {digits} maxVal: {maxVal}")
     countArr = [0 for i in range(maxVal+1)]
 
     # 1. Frequency array for count of unique elements in input array   
@@ -18,16 +16,15 @@ def countSort(arr):
     outputArr = [0 for i in range(countArr[-1])]
 
     # Traverse input array from end to preserve stability
-    for i in range(len(arr) - 1, -1, -1):
-
-        # Value at countArr[arr[i]] represents cumulative count of the element arr[i] in original array
+    # Value at countArr[arr[i]] represents cumulative count of the element arr[i] in original array
         # Cumulative count indicates total # of elements in original array that are less than or equal to arr[i]
         # -1 part is to adjust for zero-based indexing
         # EXAMPLE:
-        # If arr[i] is 3, and countArr[i] is 5, this means there are 5 elements in arr that are <= 3
+        # For arr[i] = 3 & countArr[i] =5, there are 5 elements in arr that are <= 3
         # To place 3 in sorted array, need to put it in the 4th position, so: countArr[3] - 1 = 4
-        outputArr[countArr[arr[i]] - 1] = arr[i]
-        countArr[arr[i]] -= 1 # need to decrement the cumulative count since we've already placed a number
+    for i in range(len(arr) - 1, -1, -1):
+        outputArr[countArr[arr[i]] - 1] = arr[i]    # Place numbers in correct position in outputArr
+        countArr[arr[i]] -= 1                       # Decrement cumulative count after adding a number
 
     # Print sorted array
     print(f"Sorted array: {outputArr}")
