633cd4c1c4f3dcd1c5853ee665ac30f840e5b017
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2023-11-27 12:11:59 -0600

    Stuff

--- HW4/hw4_linearSort.py
+++ HW4/hw4_linearSort.py
@@ -1,31 +1,38 @@
 # Linear Sort Algorithm
-# Sorting pixels based on color values?
 
 def countSort(arr):
     maxVal = max(arr)
     digits = len(str(maxVal))
     print(f"Digits: {digits} maxVal: {maxVal}")
 
-    outputArr = [0 for i in range(maxVal+1)]
     countArr = [0 for i in range(maxVal+1)]
-    
+
+    # Count of unique elements in countArr    
     for i in arr:
         countArr[i] += 1
-    for i in arr:
+
+    print(f"countArr: {countArr}")
+    print("------------")
+
+    # Modify freq array to store running sum of its own elements
+    for i in range(1, len(countArr)):
         countArr[i] = countArr[i - 1] + countArr[i]
     
-    for i in reversed(arr):
-        print(i)
-        print(arr[i])
-        outputArr[countArr[arr[i]]-1] = arr[i]
+    print(f"countArr: {countArr}")
+ 
+    # Create output array (list) with len equal to # of elements
+    outputArr = [0 for i in range(maxVal+1)]
+    print(outputArr)
+
+    # Put stuff into outputArr
+    for i in range(1, len(arr)):
+        print(countArr[i])
+
 
 def main():
 
-    # Create static list of RGB values
-    
     # Create test list & print
-    # data = [170, 45, 75, 90, 802, 24, 2, 66]
-    data = [0, 3, 4, 6, 3, 2, 11]
+    data = [0, 3, 4, 6, 3, 2, 7]
     print(f"Unsorted: {data}\n")
     countSort(data)
 
